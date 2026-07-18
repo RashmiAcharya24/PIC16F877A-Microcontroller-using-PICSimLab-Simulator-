144acadd98a0db78cdb043937d102995c71ba00e
RashmiAcharya24 <acharyarashmi379@gmail.com>
2026-07-18 10:01:50 +0530

    UART Sources

---
+++ 12-UART-Sources/Driver-Interrupt.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Driver-Interrupt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Driver-Interrupt.X.production.hex
